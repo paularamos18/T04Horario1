910bc64d375da22d879b06e5bbb16bb51f177460
paularamos18 <paula.ramos18@tectijuana.edu.mx>
2021-01-18 17:56:47 -0800

    Inicio

---
+++ T04Horario1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="T04Horario1.MvcApplication" Language="C#" %>
---
+++ T04Horario1/Models/Pelicula.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace T04Horario1.Models
+{
+    public class Pelicula
+    {
+        public int Id { get; set; }
+        public int Sala { get; set; }
+        public DateTime Horario { get; set; }
+        public string Clasificacion { get; set; }
+    }
+}
