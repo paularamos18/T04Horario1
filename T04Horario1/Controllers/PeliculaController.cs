803516dcca9db86ecb649c2792daea1a32eaa61f
paularamos18 <paula.ramos18@tectijuana.edu.mx>
2021-01-18 18:43:31 -0800

    2

---
+++ T04Horario1/Controllers/PeliculaController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using T04Horario1.Models;
+
+namespace T04Horario1.Controllers
+{
+    public class PeliculaController : Controller
+    {
+        private PeliculaDBConnections db = new PeliculaDBConnections();
+
+        // GET: Pelicula
+        public ActionResult Index()
+        {
+            return View(db.Peliculas.ToList());
+        }
+
+        // GET: Pelicula/Details/5
+        public ActionResult Details(int id)
+        {
+            var Pelicula = db.Peliculas.Find(id);
+
+            return View(Pelicula);
+        }
+
+        // GET: Pelicula/Create
+        public ActionResult Create(int id)
+        {
+            var Pelicula = db.Peliculas.Find(id);
+            return View(Pelicula);
+        }
+
+        // POST: Pelicula/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Sala,Horario,Clasificacion")] Pelicula pelicula)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Peliculas.Add(pelicula);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pelicula);
+        }
+
+        // GET: Pelicula/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pelicula pelicula = db.Peliculas.Find(id);
+            if (pelicula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pelicula);
+        }
+
+        // POST: Pelicula/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Sala,Horario,Clasificacion")] Pelicula pelicula)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pelicula).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pelicula);
+        }
+
+        // GET: Pelicula/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pelicula pelicula = db.Peliculas.Find(id);
+            if (pelicula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pelicula);
+        }
+
+        // POST: Pelicula/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pelicula pelicula = db.Peliculas.Find(id);
+            db.Peliculas.Remove(pelicula);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- T04Horario1/Models/PeliculaDBConnections.cs
+++ T04Horario1/Models/PeliculaDBConnections.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +9,21 @@ namespace T04Horario1.Models
 {
     public class PeliculaDBConnections:DbContext
     {
+        public PeliculaDBConnections() : base("PeliculaBd")
+        {
 
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+        }
+
+        public DbSet<Pelicula> Peliculas { get; set; }
     }
 
 }
